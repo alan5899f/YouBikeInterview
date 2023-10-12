8ffef6eb2a8a48b3a14e3ef29f04c687e858b1ac
alan <alan5899f@gmail.com>
2023-10-12 11:12:48 +0800

    Initial Commit

---
+++ Interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interview
+//
+//  Created by 陳韋綸 on 2023/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
